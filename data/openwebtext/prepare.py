b28f50f0213eef3e664cd79e2af84cfd41aac450
Jordan Walsh <iordainbreathnach@hotmail.com>
2023-06-08 20:22:24 +0100

    remove unwanted data

--- data/openwebtext/prepare.py
+++ data/openwebtext/prepare.py
@@ -1,14 +1,25 @@
 import os
 import sys
 import numpy as np
-
 from datasets import load_dataset
+
+from encoder import BPETokenizer
  
-current_dir = os.path.dirname(os.path.abspath(__file__))
+current_dir = os.path.dirname(__file__)
 home_dir = os.path.dirname(os.path.dirname(current_dir))
 sys.path.append(home_dir)
 
-from encoder import BPETokenizer
 
 data = load_dataset("openwebtext")
-split_data = data["train"]
+split_data = data["train"].train_test_split(test_size=0.0005, seed=3535, shuffle=True)
+
+tokenizer = BPETokenizer()
+
+def encode(example):
+    pass
+
+tokenized = split_data.map(function=encode, remove_columns=["text"], num_proc=8)
+
+for split, dataset in tokenized.items():
+    arr_len = np.sum(dataset["len"])
+    filename = os.path.join
