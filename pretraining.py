70dbb910dc77ef9cc9e0e0d6884627e81c7de8c4
Jordan Walsh <iordainbreathnach@hotmail.com>
2023-06-15 14:30:24 +0100

    pretraining

--- pretraining.py
+++ pretraining.py
@@ -3,9 +3,15 @@ import numpy as np
 from trainer import Trainer, TrainConfig
 from model import GPTLanguageModel, GPTConfig
 
-# Create directory where the binary files are stored
-bin_dir = os.path.join(os.path.join(os.getcwd(), "data"), "bin")
+# Access the directory where the binary files are stored
+bin_dir = os.path.join(os.path.join(os.getcwd(), "data"),"openwebtext")
 
 # Define training and validation sets
 train_data = np.memmap(os.path.join(bin_dir, "train.bin"), dtype=np.uint16, mode='r')
 val_data = np.memmap(os.path.join(bin_dir, "val.bin"), dtype=np.uint16, mode='r')
+
+# Initialize Trainer
+trainer_config = TrainConfig()
+model_config = GPTConfig()
+trainer = Trainer(config=trainer_config, model_config=model_config, train_data=train_data, val_data=val_data)
+trainer.run()
